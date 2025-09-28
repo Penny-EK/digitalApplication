e8ece6a72f0cfcfc2464fa16b905bf00f3bc4786
pekr0002 <pekr0002@stud.kea.dk>
2025-09-28 15:10:18 +0200

    funFact gen

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-document.addEventListener("DOMContentLoaded", function () {
+// themeSwithcer
+document.addEventListener("DOMContentLoaded", themeSwitcher);
+
+function themeSwitcher() {
   const themeSwitcher = document.getElementById("themeSwitcher");
   const body = document.body;
   const moonIcon = themeSwitcher.querySelector("#moonIcon");
@@ -13,4 +16,19 @@ document.addEventListener("DOMContentLoaded", function () {
       if (sunIcon) sunIcon.style.display = "none";
     }
   });
-});
+}
+
+// fun fact generator
+
+document.addEventListener("DOMContentLoaded", funFact);
+
+function funFact() {
+  const funFactDisplay = document.getElementById("funFactContainer");
+  const funFacts = ["Jeg elsker at vandre i naturen.", "I 2024 så jeg 15 forskellige koncerter.", "Jeg elsker brætspil.", "Jeg kan godt lide at lave mad.", "Jeg har en stor samling af Lego", "Min yndlingsfilm er burn after reading", "Mit yndlingsband er Red Hot Chili Peppers", "Jeg er navgivet efter filmen 'almost famous'", "Min interesse for kodning startede i gymnasiet.", "Min interesse for design startede i 5. klasse.", "Jeg kan godt lide at læse bøger, især science fiction og biografier fra rockstjerner🤘🏼.", "efter multimediedesign vil jeg gerne læse webudvikling"];
+  function displayRandomFunFact() {
+    const randomFact = Math.floor(Math.random() * funFacts.length);
+    funFactDisplay.textContent = funFacts[randomFact];
+  }
+  setInterval(displayRandomFunFact, 5000);
+  displayRandomFunFact();
+}
