9bda53d233314500f6cb634bfc7317c79cdd3c52
pekr0002 <pekr0002@stud.kea.dk>
2025-10-29 10:25:07 +0100

    fix fonts

---
+++ darkmode.js
@@ -0,0 +1,19 @@
+// themeSwithcer
+document.addEventListener("DOMContentLoaded", themeSwitcher);
+
+function themeSwitcher() {
+  const themeSwitcher = document.getElementById("themeSwitcher");
+  const body = document.body;
+  const moonIcon = themeSwitcher.querySelector("#moonIcon");
+  const sunIcon = themeSwitcher.querySelector("#sunIcon");
+  themeSwitcher.addEventListener("click", function () {
+    body.classList.toggle("darkmode");
+    if (body.classList.contains("darkmode")) {
+      if (moonIcon) moonIcon.style.display = "none";
+      if (sunIcon) sunIcon.style.display = "inline";
+    } else {
+      if (moonIcon) moonIcon.style.display = "inline";
+      if (sunIcon) sunIcon.style.display = "none";
+    }
+  });
+}
